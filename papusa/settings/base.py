771965539587cb70360bdbfcf3b9e8d373988442
Karim Tabet <Karim.Tabet@cyanconnode.com>
2017-06-02 16:39:24 +0100

    Allow all hosts

--- papusa/settings/base.py
+++ papusa/settings/base.py
@@ -17,7 +17,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [os.environ["ALLOWED_HOST"]]
+ALLOWED_HOSTS = ['*', ]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
