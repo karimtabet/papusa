fab9a3345955771bd49393e7ef17f8a072287049
Karim Tabet <Karim.Tabet@cyanconnode.com>
2017-06-23 14:38:53 +0100

    Remove unused release.sh file

--- release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo $(awk -F. '/[0-9]+\./{$NF++;print}' OFS=. <<< cat VERSION) > VERSION
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-docker push karimtabet/papusa:latest;
-docker push karimtabet/papusa:$version;
