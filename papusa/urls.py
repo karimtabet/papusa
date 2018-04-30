736c291a2a11fbd5484e20fed2b84360c87ed3b3
Karim Tabet <dev@karimtabet.com>
2018-04-30 21:24:00 +0100

    Replace include(admin.site.urls) for admin.site.urls

--- papusa/urls.py
+++ papusa/urls.py
@@ -6,19 +6,15 @@ from django.contrib import admin
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.core import urls as wagtail_urls
-from wagtail.api.v2 import urls as wagtailapi_urls
 
 from app import views
-
-
 urlpatterns = [
-    url(r'^django-admin/', include(admin.site.urls)),
+    url(r'^django-admin/', admin.site.urls),
 
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'search/$', views.search, name='search'),
-    url(r'^api/', include(wagtailapi_urls)),
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's serving mechanism
